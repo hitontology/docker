32c38970047033072c796e9e8e27b38a479c16c6
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-03-21 13:47:39 +0100

    Clean up docker-compose.yml. Add SNIK bb prefix. Use local SPARQL endpoint for LodView.

--- virtuoso/setup.sql
+++ virtuoso/setup.sql
@@ -1,3 +1,3 @@
 DB.DBA.XML_SET_NS_DECL ('hito', 'http://hitontology.eu/ontology/', 2);
+DB.DBA.XML_SET_NS_DECL ('bb', 'http://snik.eu/ontology/bb/', 2);
 DB.DBA.XML_SET_NS_DECL ('dbr', 'http://dbpedia.org/resource/', 2);
-
