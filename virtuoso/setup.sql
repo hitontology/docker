4d75c643e8c1e92936621fbe9cc60cd01275f48e
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-04-11 13:41:09 +0200

    Part of #2: Enable CORS in Virtuoso SPARQL endpoint.

--- virtuoso/setup.sql
+++ virtuoso/setup.sql
@@ -1,3 +1,4 @@
+update DB.DBA.HTTP_PATH set HP_OPTIONS = serialize(vector('browse_sheet', '', 'noinherit', 'yes', 'cors', '*', 'cors_restricted', 0))  where HP_LPATH = '/sparql';
 DB.DBA.XML_SET_NS_DECL ('hito', 'http://hitontology.eu/ontology/', 2);
 DB.DBA.XML_SET_NS_DECL ('bb', 'http://snik.eu/ontology/bb/', 2);
 DB.DBA.XML_SET_NS_DECL ('dbr', 'http://dbpedia.org/resource/', 2);
